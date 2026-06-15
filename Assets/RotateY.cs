b3722f34ed633387827a4d2949af4249466ed621
zhangjun <zjai888@gmail.com>
2026-06-15 15:11:38 +0800

    旋转加入参数

--- Assets/RotateY.cs
+++ Assets/RotateY.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class RotateY : MonoBehaviour
 {
+    public float fScaleSpeed = 1.0f;
     [SerializeField] private float rotationSpeed = 90f;
 
     private void Update()
     {
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * fScaleSpeed);
     }
 }
